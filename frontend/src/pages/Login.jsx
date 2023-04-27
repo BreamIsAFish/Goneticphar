8f71c67a968c4cf38cde2aacba66ce1336dec698
Beam <beam7684@gmail.com>
2023-04-28 04:16:38 +0700

    Improve ui of Login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,12 +1,19 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import {
+  Visibility,
+  VisibilityOff,
+  VoicemailOutlined,
+} from '@mui/icons-material'
 
 import { api } from '../utils/api'
+import { EmailOutlined } from '@mui/icons-material'
 
 const Login = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [isLoading, setLoading] = useState(false)
+  const [showPassword, setShowPassword] = useState(false)
 
   const navigate = useNavigate()
 
@@ -52,42 +59,70 @@ const Login = () => {
   }
 
   return (
-    <div className="flex flex-col justify-center items-center mx-auto my-20">
-      <h1 className="text-3xl font-bold text-pink-500">Login to Goneticphar</h1>
+    <div className="flex flex-col justify-center items-center h-screen w-screen mx-auto py-20 bg-fixed bg-cover bg-mountain">
+      <h1 className="text-4xl font-bold font-nordic text-center leading-loose text-white">
+        Login to
+        <p className="text-5xl">Goneticphar</p>
+      </h1>
 
-      <div className="flex flex-col p-8 mt-16 border border-pink-500 rounded-xl shadow-xl">
-        <h3 className="text-xl font-semibold mb-2">Username</h3>
-        <input
-          type="text"
-          onChange={(e) => setUsername(e.target.value)}
-          className="border rounded-lg w-full px-4 py-2"
-        />
-        <h3 className="text-xl font-semibold mb-2 mt-4">Password</h3>
-        <input
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-          className="border rounded-lg w-full px-4 py-2"
-        />
+      {/* Login Form */}
+      <div className="flex flex-col p-16 mt-16 border bg-white bg-opacity-20 border-white rounded-3xl shadow-xl">
+        {/* Username Field */}
+        <h3 className="text-xl font-semibold font-aqua text-white mb-2">
+          Username
+        </h3>
+        <div className="flex flex-row justify-between items-center border-b">
+          <input
+            type="text"
+            placeholder="Username"
+            onChange={(e) => setUsername(e.target.value)}
+            className="text-lg font-medium font-aqua text-white w-full px-4 py-2 bg-transparent outline-none"
+          />
+          <EmailOutlined color="action" />
+        </div>
+
+        {/* Password Field */}
+        <h3 className="text-xl font-semibold font-aqua text-white mb-2 mt-4">
+          Password
+        </h3>
+        <div className="flex flex-row justify-between items-center border-b">
+          <input
+            type={showPassword ? 'text' : 'password'}
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            className="text-lg font-medium font-aqua text-white w-full px-4 py-2 bg-transparent outline-none"
+          />
+          <div onClick={() => setShowPassword((prev) => !prev)}>
+            {showPassword ? (
+              <VisibilityOff color="action" />
+            ) : (
+              <Visibility color="action" />
+            )}
+          </div>
+        </div>
 
         {/* Button Section */}
-        <div className="flex flex-col w-full mt-8">
+        <div className="flex flex-col justify-center items-center w-full mt-8">
           <button
             onClick={login}
             disabled={!isInputValid() || isLoading}
-            className={`font-bold p-4 mt-4 w-96 rounded-xl ${
+            className={`font-bold py-3 mt-4 w-60 rounded-full ${
               !isInputValid()
-                ? `bg-gray-300 text-gray-100`
-                : `bg-pink-500 text-white`
+                ? `bg-gray-300 bg-opacity-50 text-gray-500`
+                : `bg-purple-700 text-white`
             }`}
           >
-            {isLoading ? 'Loading...' : 'Login'}
+            {isLoading ? 'Loading...' : 'Log in'}
           </button>
-          <button
-            onClick={register}
-            className="font-bold p-4 mt-4 w-96 border-2 rounded-xl"
-          >
-            Create new account
-          </button>
+          <p className="flex text-base font-aqua font-bold text-white mt-8">
+            Don't have an account?{' '}
+            <p
+              onClick={register}
+              className="pl-2 underline cursor-pointer"
+            >
+              Create an account
+            </p>
+          </p>
         </div>
       </div>
     </div>
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,27 +1,27 @@
-import axios from "axios";
+import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8000",
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000',
   // timeout: 20000,
   // withCredentials: true,
-});
+})
 
 api.interceptors.request.use((request) => {
-  const token = localStorage.getItem("token");
-  if (token) request.headers["token"] = token;
-  return request;
-});
+  const token = localStorage.getItem('token')
+  if (token && !request.url.includes('/user')) request.headers['token'] = token
+  return request
+})
 
 api.interceptors.response.use(
   (response) => {
-    return response;
+    return response
   },
   (error) => {
     if (error.response?.status === 401) {
-      console.log("token cleared");
-      localStorage.removeItem("token");
-      console.log("Token expired or invalid");
+      console.log('token cleared')
+      localStorage.removeItem('token')
+      console.log('Token expired or invalid')
     }
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-);
+)
