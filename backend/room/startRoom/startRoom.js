228ad50af7f00acea0709accce9951bfd2cb9fa1
kharit <qwertyunity123@gmail.com>
2023-05-01 21:16:37 +0700

    update startroom

--- backend/room/startRoom/startRoom.js
+++ backend/room/startRoom/startRoom.js
@@ -8,18 +8,16 @@ admin.initializeApp({
 });
 
 exports.handler = async (event, context, callback) => {
-    const vocabulary = ["Crab", "Hat", "Bird", "Sheep", "Car",
-						"Elephant", "Glasses", "Pants", "Turtle", "Table"]
+
+    var question_list;
     const room = admin.firestore().collection("room");
-    const question_list = generateRandomQuestionList(vocabulary);
     await room.where("room_num", '==', event.room_num).get().then((querySnapshot) => {
       const doc = querySnapshot.docs[0];
-      // question_list = doc.data().question_list;
+      question_list = doc.data().question_list;
       doc.ref.update({
-        "room_status": "playing",
-        "question_list": question_list
+        "room_status": "playing"
       });
-	  })
+	})
 
     startQuestion(event.room_num, question_list, 1);
 
@@ -67,20 +65,6 @@ async function startQuestion(room_num, question_list, question_no) {
 exports.nextQuestion = async (room_num, question_list, question_no) => {
   console.log(`nextQuestion called...${question_no}`);
 
-  // TODO: Remove call createMessage
-  //   const lambda = new AWS.Lambda();
-  //   // Set up the input parameters for the other Lambda function
-  //   const params = {
-  //     FunctionName: 'arn:aws:lambda:ap-southeast-1:723746594407:function:createMessage',
-  //     InvocationType: 'RequestResponse',
-
-  //     Payload: JSON.stringify({sender: 'Buay', room_id: 2, message: `${questionList[question_no-1]}`})
-  //   };
-  //   // Invoke the other Lambda function
-  //   const response = await lambda.invoke(params).promise();
-  //   // Handle the response from the other Lambda function
-  //   console.log(response);
-
   if(question_no == question_list.length) {
     // TODO: Change room status to "ending"
     const room = admin.firestore().collection("room");
@@ -95,12 +79,3 @@ exports.nextQuestion = async (room_num, question_list, question_no) => {
   }
 
 };
-
-function generateRandomQuestionList(vocabulary) {
-	const randomItems = [];
-	while (randomItems.length < 5) {
-		const index = Math.floor(Math.random() * vocabulary.length);
-		randomItems.push(vocabulary.splice(index, 1)[0]);
-	}
-	return randomItems;
-}
